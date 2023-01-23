91d92b16fba69850394627d2d323c23f34572357
aladdin4u <taofikakinkumi@gmail.com>
2023-01-23 14:19:23 +0100

    configured airtable proxy

---
+++ worker/src/handler.js
@@ -0,0 +1,72 @@
+import { isAllowed } from "./is-allowed";
+import { getMethod } from "./get-method";
+import { getTarget } from "./get-target";
+
+const config = { 
+  'airtableApiUrl': 'https://api.airtable.com',
+  'airtableBaseId': 'AIRTABLE_API_BASE_ID',
+  'airtableApiVersion': 'v0',
+  'airtableApiKey': 'AIRTABLE_API_BASE_ID',
+  'cacheTime': 0,
+  'prefix': '',
+  'allowedTargets': '*',
+};
+const parseJson = obj => {
+  try {
+    return JSON.parse(obj);
+  } catch (e) {
+    return obj;
+  }
+};
+
+export async function handleRequest(req) {
+  try {
+    const method = getMethod(req);
+    const target = getTarget(req, config);
+
+    if (
+      !isAllowed(
+        method,
+        target.airtableResource,
+        parseJson(config.allowedTargets)
+      )
+    ) {
+      return new Response("Method Not Allowed", {
+        status: 405,
+        statusText: `Method "${method}" not allowed on "${
+          target.airtableResource
+        }" resource`
+      });
+    }
+
+    const response = await fetch(target.airtableRequestUrl, {
+      headers: {
+        Authorization: `Bearer ${config.airtableApiKey}`,
+        "Content-type": "application/json"
+      },
+      method: method,
+      body: req.body
+    });
+
+    const body = await response.body;
+
+    const headers = new Headers();
+    for (const kv of response.headers.entries()) {
+      headers.append(kv[0], kv[1]);
+    }
+    headers.set("Cache-Control", "max-age=" + config.cacheTime);
+
+    return new Response(body, {
+      status: response.status,
+      statusText: response.statusText,
+      headers: headers
+    });
+  } catch (e) {
+    console.error(e);
+
+    return new Response("Bad Request", {
+      status: 400,
+      statusText: "Bad Request"
+    });
+  }
+}
